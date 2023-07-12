a394f1c4684fee3a06470f3eeee5413a70497814
Иван <ivanzhurenko@gmail.com>
2023-07-12 13:34:36 +0500

    Revert "Выполнил задание 2.1.2"
    This reverts commit 21651cc4ab1562b3d1e8a77f87cc0db626cb3aae.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,14 +5,8 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld hwBean =
+        HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
-        HelloWorld hwBean2 =
-                (HelloWorld) applicationContext.getBean("helloworld");
-        Cat catBean = (Cat) applicationContext.getBean("cat");
-        Cat catBean2 = (Cat) applicationContext.getBean("cat");
-
-        System.out.println("Hello world equal? " + hwBean2.equals(hwBean));
-        System.out.println("Cats equal? " + catBean2.equals(catBean));
+        System.out.println(bean.getMessage());
     }
 }
